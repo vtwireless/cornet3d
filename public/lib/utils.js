9dbf89d126cc5c9981128e768a78fd8ee454b94d
Lance Arsenault <lanceman@vt.edu>
2017-06-19 15:03:14 -0400

    dos file to unix format

--- public/lib/utils.js
+++ public/lib/utils.js
@@ -4,9 +4,14 @@
 // This is just so we get feedback when things go wrong:
 function Fail(arg) {
 
-    var prefix = 'ERROR: Likely a coding error:\n   ';
-    console.log(prefix + arg);
-    alert(prefix + arg);
+    var text = 'ERROR: Likely a coding error:\n   ' +
+        arg;
+    var line = '-----------------------------------------------\n';
+    text += '\n' + line + '  STACK TRACE\n' + line +
+        (new Error()).stack + line
+
+    console.log(text);
+    alert(text);
     throw 'FAIL';
 }
 
@@ -25,72 +30,69 @@ function GetElementById(id) {
 
 
 function selOverlay(canvas, leftMargin, rightMargin){
-	var thisObject={};
+    var thisObject={};
     function getMousePos(canvas, evt) {
-      	var rect = canvas.getBoundingClientRect();
+        var rect = canvas.getBoundingClientRect();
         return {
-          	x: evt.clientX - rect.left,
-          	y: evt.clientY - rect.top
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
         };
     }
 
     var context = canvas.getContext('2d');
-	var pt1x=10;
+    var pt1x=10;
     var state=0;
     var x;
     thisObject.selLow=0;
     thisObject.selHight=0;
 
     canvas.addEventListener('mousemove', function(evt) {
-    	var mousePos = getMousePos(canvas, evt);
+        var mousePos = getMousePos(canvas, evt);
         x=mousePos.x;
-		fixOutside();
-    	if (state===0) {
-    		context.clearRect(0, 0, canvas.width, canvas.height);
-	        context.fillStyle = "rgba(255, 0, 255, 0.5)";
-	        context.fillRect(x-1, 0, 2, canvas.height);
-    	} else if (state===1) {
-	        context.clearRect(0, 0, canvas.width, canvas.height);
-	        context.fillStyle = "rgba(255, 0, 255, 0.5)";
-	        context.fillRect(pt1x, 0, x-pt1x, canvas.height);
-	    }
+	fixOutside();
+        if(state===0) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+	    context.fillStyle = "rgba(255, 0, 255, 0.5)";
+	    context.fillRect(x-1, 0, 2, canvas.height);
+        } else if (state===1) {
+	    context.clearRect(0, 0, canvas.width, canvas.height);
+	    context.fillStyle = "rgba(255, 0, 255, 0.5)";
+	    context.fillRect(pt1x, 0, x-pt1x, canvas.height);
+	}
     }, false);
 
     canvas.addEventListener('click', function(evt) {
-    	var mousePos = getMousePos(canvas, evt);
-    	x=mousePos.x;
-		
-		fixOutside();
-    	if (state===0 || state===2) {
-	    	pt1x=x;
-	    	state=1;
-	    } else if (state===1){
-	    	state=2;
-	    	thisObject.selLow = ((pt1x-leftMargin)/(canvas.width-leftMargin-rightMargin));
-	    	thisObject.selHigh = ((x-leftMargin)/(canvas.width-leftMargin-rightMargin));
-	    	/*var message = 'Selected frequency range fractions: ' +
-                  thisObject.selLow.toFixed(2) + ' ,' +
-                  thisObject.selHigh.toFixed(2) + '.'; */
-            
+        var mousePos = getMousePos(canvas, evt);
+        x=mousePos.x;
+        fixOutside();
+        if(state===0 || state===2) {
+	    pt1x=x;
+	    state=1;
+        } else if (state===1) {
+	    state=2;
+	    thisObject.selLow = ((pt1x-leftMargin)/(canvas.width-leftMargin-rightMargin));
+	    thisObject.selHigh = ((x-leftMargin)/(canvas.width-leftMargin-rightMargin));
+	    /*var message = 'Selected frequency range fractions: ' +
+              thisObject.selLow.toFixed(2) + ' ,' +
+              thisObject.selHigh.toFixed(2) + '.'; */
+
             var bandwidthF=(thisObject.selHigh-thisObject.selLow)*spectrum.bandwidth_;
             var centerF=spectrum.lowBound_+thisObject.selLow*spectrum.bandwidth_+bandwidthF/2;
             spectrum.selectedCenter=centerF;
             spectrum.selectedBandWidth=bandwidthF;
             var message = 'Center: ' +  centerF.toFixed(2) + 'MHz.  Bandwidth: ' + bandwidthF.toFixed(2) + ' MHz.';
-	        context.font = '18pt Calibri';
-	        context.fillStyle = 'black';
-	        context.fillText(message, (canvas.width/2), 25);
-	        document.getElementById('transmit_button').disabled=false;
-	    }
+	    context.font = '18pt Calibri';
+	    context.fillStyle = 'black';
+	    context.fillText(message, (canvas.width/2), 25);
+	    document.getElementById('transmit_button').disabled=false;
+	}
     }, false);
-    
+
     function fixOutside(){
-    	if (x<leftMargin) {
-        	x=leftMargin;
-        }
-        if (x>(canvas.width-rightMargin)) {
-        	x=canvas.width-rightMargin;
-        }
+        if (x<leftMargin)
+            x=leftMargin;
+        if (x>(canvas.width-rightMargin))
+            x=canvas.width-rightMargin;
     }
     /*
     thisObject.fitToContainer = function(){
