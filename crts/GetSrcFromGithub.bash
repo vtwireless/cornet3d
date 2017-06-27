94d8e7b5502faf7ed9c3398e9d7a55e74f289dd8
Lance Arsenault <lanceman@vt.edu>
2017-06-27 12:48:28 -0400

    fixing build and install

---
+++ crts/GetSrcFromGithub.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+set -ex
+set -o pipefail
+
+
+#Usage: GetSrcFromGithub user package tag [sha512]
+
+function GetSrcFromGithub()
+{
+    [ -n "$3" ] || exit 1
+
+    local cwd="$PWD"
+    local name="$1-$2"
+    local path="$1/$2"
+    local tag="$3"
+    local tarfile=$cwd/$1-$2-ZxxX_src.tar.gz
+
+    # This gets a tarball file from github for the package
+    wget -O $tarfile https://github.com/$path/tarball/$tag
+
+    if [ -n "$4" ] ; then
+        # Check that the file has a good SHA512 sum
+        echo "$4  $tarfile" | sha512sum -c
+    fi
+
+    local tmpdir=$(mktemp -d --suffix=$name)
+
+    cd $tmpdir
+    tar -xzf $tarfile
+    mv ${name}* $cwd/src
+    rmdir $tmpdir
+    cd -
+    mv $tarfile src
+}
